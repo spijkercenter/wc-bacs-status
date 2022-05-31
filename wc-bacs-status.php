2c2770eebe57b33de5b1eaf74bbe2ca345366c89
Ivo Spijkerman <ivo@spijkerman.nl>
2022-05-31 14:18:53 +0200

    initial commit

---
+++ wc-bacs-status.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  WooCommerce BACS order status
+Plugin URI:   https://github.com/spijkercenter/wc-bacs-status
+Description:  A plugin that sets the initial order state of BACS orders to awaiting_payment
+Version:      0.1
+Author:       drivol
+Author URI:   https://github.com/spijkercenter
+License:      MIT
+License URI:  https://mit-license.org/
+Text Domain:  wc-bacs-status
+Domain Path:  /languages
+*/
